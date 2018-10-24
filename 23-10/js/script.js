49e2ac48cfe07a5e8c76727fef5bf36743ab7387
nhatdaica05555 <nhatdaica05555>
2018-10-24 09:59:36 +0700

    add 23/10

--- 23-10/js/script.js
+++ 23-10/js/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-    
+    $('.toggle').click(function (e) { 
+        e.preventDefault();
+        isLight = $('.light').attr('src');
+        if(isLight === "img/pic_bulboff.gif"){
+            $('.light').attr('src',"img/pic_bulbon.gif");
+            $('.toggle').text('Tắt')
+            $('.toggle').css('background' , '#00BCD4');
+        }else{
+            $('.light').attr('src',"img/pic_bulboff.gif");
+            $('.toggle').text('Bật')
+            $('.toggle').css('background' , ' none');
+        }
+    });
 })
